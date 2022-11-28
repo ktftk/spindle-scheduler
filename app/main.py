ed0503b5510c348a9bc9690be4aeb796b6b498d1
ktftk <62935224+ktftk@users.noreply.github.com>
2022-11-28 20:34:28 +0900

    feat: add create-scheduled-spinners

--- app/main.py
+++ app/main.py
@@ -1,16 +1,38 @@
+import json
+import logging
+from typing import Optional
+
 from fastapi import Depends, FastAPI
 from psycopg2._psycopg import connection
 
 from .domains import ReleaseReminder
+from .publisher import get_topic_path, publisher
 from .repository import get_conn, read_releases_to_remind
+
+logger = logging.getLogger("uvicorn")
+logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
 
 
 @app.post("/release-reminders", response_model=list[ReleaseReminder])
-def root(conn: connection = Depends(get_conn)):
+def create_release_reminders(
+    conn: connection = Depends(get_conn),
+    topic_path: Optional[str] = Depends(get_topic_path),
+):
     releases = read_releases_to_remind(conn)
     release_reminders = [
         ReleaseReminder(release=release) for release in releases
     ]
+    if topic_path is not None:
+        for release_reminder in release_reminders:
+            publisher.publish(
+                topic_path, json.dumps(release_reminder.dict()).encode("utf-8")
+            )
     return release_reminders
+
+
+@app.post("/release-reminder-receivers/create-scheduled-spinners")
+def create_scheduled_spinners(message):
+    logger.info(message)
+    return message
---
+++ app/publisher.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from google.cloud import pubsub_v1
+
+from .config import PUBSUB_PROJECT_ID, PUBSUB_TOPIC_ID
+
+publisher = pubsub_v1.PublisherClient()
+
+
+def get_topic_path() -> Optional[str]:
+    if PUBSUB_PROJECT_ID is None or PUBSUB_TOPIC_ID is None:
+        return None
+    return publisher.topic_path(PUBSUB_PROJECT_ID, PUBSUB_TOPIC_ID)
+
+
+# def publish_release_reminder(release_reminder: ReleaseReminder) -> None:
+#     publisher.publish(
+#         topic_path, json.dumps(release_reminder.dict()).encode("utf-8")
+#     )
