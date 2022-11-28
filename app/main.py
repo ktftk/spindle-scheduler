c1127b85df7eb3c218874528563d9d37a100d144
ktftk <62935224+ktftk@users.noreply.github.com>
2022-11-28 16:10:37 +0900

    chore: rm publisher

--- app/main.py
+++ app/main.py
@@ -1,30 +1,27 @@
-from typing import Callable
+import logging
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Request
 from psycopg2._psycopg import connection
 
 from .domains import ReleaseReminder
-from .publisher import publish_release_reminder
 from .repository import get_conn, read_releases_to_remind
+
+logger = logging.getLogger("uvicorn")
+logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
 
 
-def get_publish_release_reminder() -> Callable[[ReleaseReminder], None]:
-    return publish_release_reminder
-
-
 @app.post("/release-reminders", response_model=list[ReleaseReminder])
 def root(
+    request: Request,
     conn: connection = Depends(get_conn),
-    publish_release_reminder: Callable[[ReleaseReminder], None] = Depends(
-        get_publish_release_reminder
-    ),
 ):
+    logger.info(
+        {"Requested": {"Method": request.method, "URL": str(request.url)}}
+    )
     releases = read_releases_to_remind(conn)
     release_reminders = [
         ReleaseReminder(release=release) for release in releases
     ]
-    for release_reminder in release_reminders:
-        publish_release_reminder(release_reminder)
     return release_reminders
