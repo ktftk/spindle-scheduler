752007ca651ae484f480df1b636c0640a13238dc
ktftk <62935224+ktftk@users.noreply.github.com>
2022-11-28 18:21:48 +0900

    refactor: rm logging

--- app/main.py
+++ app/main.py
@@ -1,25 +1,14 @@
-import logging
-
-from fastapi import Depends, FastAPI, Request
+from fastapi import Depends, FastAPI
 from psycopg2._psycopg import connection
 
 from .domains import ReleaseReminder
 from .repository import get_conn, read_releases_to_remind
 
-logger = logging.getLogger("uvicorn")
-logger.setLevel(logging.DEBUG)
-
 app = FastAPI()
 
 
 @app.post("/release-reminders", response_model=list[ReleaseReminder])
-def root(
-    request: Request,
-    conn: connection = Depends(get_conn),
-):
-    logger.info(
-        {"Requested": {"Method": request.method, "URL": str(request.url)}}
-    )
+def root(conn: connection = Depends(get_conn)):
     releases = read_releases_to_remind(conn)
     release_reminders = [
         ReleaseReminder(release=release) for release in releases
