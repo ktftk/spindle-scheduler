1162e3ebfb096eb8519309eed646035fffb58e67
ktftk <62935224+ktftk@users.noreply.github.com>
2023-01-03 09:34:49 +0900

    fix: setup

--- setup.py
+++ setup.py
@@ -4,4 +4,7 @@ setup(
     name="spindle-schduler",
     version="0.0.1",
     packages=find_packages(),
+    include_package_data=True,
+    install_requires=open("requirements.txt").read().splitlines(),
+    tests_require=["pytest"],
 )
