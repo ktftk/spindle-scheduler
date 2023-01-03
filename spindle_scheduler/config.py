bc89a3d62af1d41e2486aabaafaab2c56699571b
ktftk <62935224+ktftk@users.noreply.github.com>
2023-01-03 09:38:24 +0900

    fix: config env

--- spindle_scheduler/config.py
+++ spindle_scheduler/config.py
@@ -1,11 +1,8 @@
 import os
-from pathlib import Path
-
-from dotenv import load_dotenv
-
-load_dotenv(Path(__file__).parent.parent)
-
 
 DB_URL = os.environ["DB_URL"]
 
 DB_SCHEMA = os.environ["DB_SCHEMA"]
+
+
+SPIDER_RUN_TASK_EXECUTION_ADVANCE_TIME = 300
